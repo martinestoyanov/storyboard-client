203d25f28c2fc0bb2b87d1049ce1a9114ecf5dec
Keith <keithrichardmcgowan@gmail.com>
2021-06-12 16:47:54 -0400

    Routes created in paths.js and used on home page.

--- src/components/Storyboard/Storyboard.jsx
+++ src/components/Storyboard/Storyboard.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./Storyboard.css";
 import { Link } from "react-router-dom";
+import * as PATHS from "../../utils/paths";
 
 export default class Storyboard extends Component {
   render() {
     return (
       <div className="storyboard">
         <div className="panel">
-          <Link to="" className="panel-link">
+          <Link to={PATHS.TOPCONTENT} className="panel-link">
             <h2>Top Content</h2>
             <img
               src="https://cdn5.vectorstock.com/i/1000x1000/39/99/business-growth-bar-graph-finance-increase-vector-17313999.jpg"
@@ -16,7 +17,7 @@ export default class Storyboard extends Component {
           </Link>
         </div>
         <div className="panel">
-          <Link to="" className="panel-link">
+          <Link to={PATHS.WRITESTORY} className="panel-link">
             <h2>Write a Story</h2>
             <img
               src="https://t3.ftcdn.net/jpg/03/14/27/00/360_F_314270083_ClBUF6NKCgniaj8CLQ6vEnY8huXImgVp.jpg"
@@ -25,7 +26,7 @@ export default class Storyboard extends Component {
           </Link>
         </div>
         <div className="panel">
-          <Link to="" className="panel-link">
+          <Link to={PATHS.INSPIRATION} className="panel-link">
             <h2>Look for Inspiration</h2>
             <img
               src="https://lh3.googleusercontent.com/proxy/8LJV5pOU3wxADvTZB2LdPtazK0gkuGp_xMBMROD-_KeNnz58uv8rqASg1ME73iX1ZypXl8W_uBN3eaDYzQjte2PAQw"
@@ -37,49 +38,55 @@ export default class Storyboard extends Component {
           <h2>Genres</h2>
           <div className="genre-panel">
             <div>
-              <Link to="" className="panel-link genre-link">
+              <Link
+                to={PATHS.ACTIONADVENTURE}
+                className="panel-link genre-link"
+              >
                 <p>
                   <b>Action/Adventure</b>
                 </p>
               </Link>
-              <Link to="" className="panel-link genre-link">
+              <Link to={PATHS.COMEDY} className="panel-link genre-link">
                 <p>
                   <b>Comedy</b>
                 </p>
               </Link>
-              <Link to="" className="panel-link genre-link">
+              <Link to={PATHS.DRAMA} className="panel-link genre-link">
                 <p>
                   <b>Drama</b>
                 </p>
               </Link>
-              <Link to="" className="panel-link genre-link">
+              <Link to={PATHS.FANTASY} className="panel-link genre-link">
                 <p>
                   <b>Fantasy</b>
                 </p>
               </Link>
-              <Link to="" className="panel-link genre-link">
+              <Link to={PATHS.HORROR} className="panel-link genre-link">
                 <p>
                   <b>Horror</b>
                 </p>
               </Link>
             </div>
             <div>
-              <Link to="" className="panel-link genre-link">
+              <Link to={PATHS.ROMANCE} className="panel-link genre-link">
                 <p>
                   <b>Romance</b>
                 </p>
               </Link>
-              <Link to="" className="panel-link genre-link">
+              <Link to={PATHS.ROMCOM} className="panel-link genre-link">
                 <p>
                   <b>RomCom</b>
                 </p>
               </Link>
-              <Link to="" className="panel-link genre-link">
+              <Link to={PATHS.SCIFI} className="panel-link genre-link">
                 <p>
                   <b>Sci-Fi</b>
                 </p>
               </Link>
-              <Link to="" className="panel-link genre-link">
+              <Link
+                to={PATHS.THRILLERMYSTERY}
+                className="panel-link genre-link"
+              >
                 <p>
                   <b>Thriller/Mystery</b>
                 </p>
@@ -88,7 +95,7 @@ export default class Storyboard extends Component {
           </div>
         </div>
         <div className="panel">
-          <Link to="" className="panel-link">
+          <Link to={PATHS.RANDOM} className="panel-link">
             <h2>Random</h2>
             <img
               src="https://www.pngrepo.com/png/323837/180/card-random.png"
@@ -97,7 +104,7 @@ export default class Storyboard extends Component {
           </Link>
         </div>
         <div className="panel">
-          <Link to="" className="panel-link">
+          <Link to={PATHS.PROFILEPAGE} className="panel-link">
             <h2>Profile Page</h2>
             <img
               src="https://lh3.googleusercontent.com/proxy/v2TBQHnRpaJ3iyNTaYiTninbntQROsXhdfBLxGW2rLlVgl632a-fqKa6eCFWP3pRJk_U5sFU9RHLI3dhz3lp0NfZnM9WaVqwOia7eHyc9-RmO3rsv8zm5GvmflBbG2lhy-fggYHpnwXb"
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -23,49 +23,6 @@ function HomePage() {
       <div>
         <Storyboard />
       </div>
-      {/* <div>
-        <div>
-          <h2>Top Content</h2>
-          <img src="" alt="Top Content" />
-        </div>
-        <div>
-          <h2>Write a Story</h2>
-          <img
-            src="https://t3.ftcdn.net/jpg/03/14/27/00/360_F_314270083_ClBUF6NKCgniaj8CLQ6vEnY8huXImgVp.jpg"
-            alt="Write a Story"
-          />
-        </div>
-        <div>
-          <h2>Look for Inspiration</h2>
-          <img
-            src="https://lh3.googleusercontent.com/proxy/8LJV5pOU3wxADvTZB2LdPtazK0gkuGp_xMBMROD-_KeNnz58uv8rqASg1ME73iX1ZypXl8W_uBN3eaDYzQjte2PAQw"
-            alt="Look for Inspiration"
-          />
-        </div>
-        <div>
-          <h2>Genres</h2>
-          <p>Action/Adventure</p>
-          <p>Comedy</p>
-          <p>Drama</p>
-          <p>Fantasy</p>
-          <p>Horror</p>
-          <p>Romance</p>
-          <p>RomCom</p>
-          <p>Sci-Fi</p>
-          <p>Thriller/Mystery</p>
-        </div>
-        <div>
-          <h2>Random</h2>
-          <img src="" alt="Random" />
-        </div>
-        <div>
-          <h2>Profile Page</h2>
-          <img
-            src="https://lh3.googleusercontent.com/proxy/v2TBQHnRpaJ3iyNTaYiTninbntQROsXhdfBLxGW2rLlVgl632a-fqKa6eCFWP3pRJk_U5sFU9RHLI3dhz3lp0NfZnM9WaVqwOia7eHyc9-RmO3rsv8zm5GvmflBbG2lhy-fggYHpnwXb"
-            alt="Profile Page"
-          />
-        </div>
-      </div> */}
     </div>
   );
 }

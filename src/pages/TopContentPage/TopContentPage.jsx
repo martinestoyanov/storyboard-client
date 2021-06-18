2e3a1f1a82bcad99e0f469283cce34acb5f11454
Martin Stoyanov <martinestoyanov@gmail.com>
2021-06-17 22:17:04 -0400

    Added Story and skeletons

--- src/pages/TopContentPage/TopContentPage.jsx
+++ src/pages/TopContentPage/TopContentPage.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import "./TopContentPage.css"
+import StoryDisplay from '../../components/Story/StoryDisplay'
 
 export default class TopContentPage extends Component {
     render() {
         return (
             <div>
                 <h2>This is the Top Content Page</h2>
+                {/* Line below is for testing individual components */}
+                <StoryDisplay id="60ca3b88b9b97df2497d244d"/>
             </div>
         )
     }

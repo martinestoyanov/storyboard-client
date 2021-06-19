3b2f48c50b973c6f5a3b96d9b60d0d436a254bcc
Keith <keithrichardmcgowan@gmail.com>
2021-06-19 12:57:05 -0400

    Inspiration components.

---
+++ src/components/Inspiration/NoVideoInspiration.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Inspiration.css";
+
+export default class NoVideoInspiration extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Stories with 0 Video Comments</h2>
+        <p>List of 5 videos with 0 comments.</p>
+      </div>
+    );
+  }
+}

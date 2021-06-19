a01f7df30ceed8846df59109f37b6a25c4ab40fb
Martin Stoyanov <martinestoyanov@gmail.com>
2021-06-19 17:52:27 -0400

    Un RickRoll

--- src/components/Video/VideoDisplay.jsx
+++ src/components/Video/VideoDisplay.jsx
@@ -20,34 +20,9 @@ export default class VideoDisplay extends Component {
         <ReactPlayer
           url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           controls
-          muted={this.muted}
-          playing
-          onPlay={this.autoUnMute}
         />
       </div>
     );
   }
 }
 
-// import React, { useState } from 'react'
-// import ReactPlayer from 'react-player/lazy'
-
-// const MyPlayer = () => {
-
-//     const [ playState, setPlayState ] = useState(false);
-
-//     const yourFunction = (event) => {
-
-//         console.log('Your event: ', event);
-
-//     };
-
-//     return (
-//         <ReactPlayer
-//             url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-//             playing={playState}
-//             onPlay={yourFunction}/>
-//     );
-
-// }
-// export default MyPlayer;

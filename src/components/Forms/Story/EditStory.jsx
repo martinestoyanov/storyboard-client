35ec86cb7734f33617f296cbef09c38fea1bd7a0
Keith <keithrichardmcgowan@gmail.com>
2021-06-27 11:25:32 -0400

    EditStory

---
+++ src/components/Forms/Story/EditStory.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import "./Story.css"
+
+export default class EditStory extends Component {
+    state = {}
+
+
+
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                <form onSubmit={this.submitHandler} className="story-form">
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={this.state.title}
+            onChange={this.changeHandler}
+            className="story-form-title"
+          />
+          <select
+            name="genre"
+            value={this.state.value}
+            onChange={this.changeHandler}
+          >
+            <option value="Action/Adventure">Action/Adventure</option>
+            <option value="Comedy">Comedy</option>
+            <option value="Drama">Drama</option>
+            <option value="Fantasy">Fantasy</option>
+            <option value="Horror">Horror</option>
+            <option value="Romance">Romance</option>
+            <option value="Romantic Comedy">Romantic Comedy</option>
+            <option value="Sci-Fi">Sci-Fi</option>
+            <option value="Thriller/Mystery">Thriller/Mystery</option>
+          </select>
+          <textarea
+            name="text"
+            id="text"
+            placeholder="Start your story here."
+            value={this.state.text}
+            onChange={this.changeHandler}
+            // rows="26"
+          ></textarea>
+          <div>
+            <button type="submit">Post Story</button>
+          </div>
+        </form>
+            </div>
+        )
+    }
+}
---
+++ src/pages/WriteStoryPage/EditStoryPage.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import EditStory from "../../components/Forms/Story/EditStory";
+import "./WriteStoryPage.css";
+
+export default class EditStoryPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Edit Your Story</h2>
+        <div className="write-story">
+          <div className="story-directions">
+            <p>
+              Let's make some edits!
+            </p>
+          </div>
+          <div className="story-form">
+            <EditStory {...this.props} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
